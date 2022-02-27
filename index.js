23e7d20226cf9dc59e8fb32f8f36a92182bdcacb
agustin-wet <wet.4gustin@gmail.com>
2022-02-27 01:50:24 -0300

    first commit whit minimal server

---
+++ index.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+const hostname = '172.31.91.64';
+const port = 8666;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('AWS test server made with node, if you recieve this message im working!!!');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
